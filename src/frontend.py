e6360626fc2994dea86c444a7135ed093b474680
buonhobo <bnnalx@gmail.com>
2024-09-30 11:55:39 +0200

    moved title left

--- src/frontend.py
+++ src/frontend.py
@@ -84,7 +84,6 @@ class MainWindow(Gtk.ApplicationWindow):
         content = Adw.ToolbarView()
         header = Adw.HeaderBar()
         header.set_show_title(True)
-        header.set
         content.add_top_bar(header)
         content.set_content(main_box)
         return content
