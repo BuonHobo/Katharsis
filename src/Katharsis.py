819476da1f412394b84687c630dcf6f1b9c84c4e
buonhobo <bnnalx@gmail.com>
2024-10-02 23:06:40 +0200

    added copy pasting to first terminal

--- src/Katharsis.py
+++ src/Katharsis.py
@@ -8,19 +8,33 @@ gi.require_version("Adw", "1")
 gi.require_version("Vte", "3.91")
 from gi.repository import Adw
 from UI.MainWindow import MainWindow
+from UI.Terminal import Terminal
 
 
 class MyApp(Adw.Application):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.connect("activate", self.on_activate)
+        self.connect("startup", print, "startup")
+
+        self.create_action('copy', Terminal.on_copy, ['<Ctrl><Shift>c'])
+        self.create_action('paste', Terminal.on_paste, ['<Ctrl><Shift>v'])
 
     def on_activate(self, app):
         MainWindow(application=app).present()
 
+    def create_action(self, name, callback, shortcuts):
+        action = Gio.SimpleAction.new(name=name, parameter_type=None)
+        action.connect('activate', callback)
+        self.add_action(action=action)
+        self.set_accels_for_action(
+            detailed_action_name=f'app.{name}',
+            accels=shortcuts,
+        )
+
 
 app = MyApp(
     application_id="io.github.buonhobo.Katharsis",
-    flags=Gio.ApplicationFlags.NON_UNIQUE,
+    flags=Gio.ApplicationFlags.DEFAULT_FLAGS,
 )
 app.run(sys.argv)
--- src/UI/Terminal.py
+++ src/UI/Terminal.py
@@ -1,12 +1,35 @@
+from __future__ import annotations
+
 from typing import Optional
 
 from gi.repository import Vte, GLib
 
 
 class Terminal(Vte.Terminal):
+    focused: Terminal = None
+
     def __init__(self):
-        super().__init__(margin_top=5, margin_bottom=20, margin_start=30, margin_end=20, bold_is_bright=True)
+        super().__init__(margin_top=5, margin_bottom=20, margin_start=20, margin_end=20, bold_is_bright=True)
         self.set_clear_background(False)
+        if self.focused is None:
+            Terminal.focused = self
+        self.connect("notify::has-focus", self.on_focus_changed)
+
+    def on_focus_changed(self, widget: Terminal, v):
+        if widget.has_focus():
+            Terminal.focused = widget
+            print(str(Terminal.focused), "is focused")
+
+    @classmethod
+    def on_copy(cls, a, b):
+        print("a")
+        Terminal.focused.copy_clipboard_format(Vte.Format.TEXT)
+
+    @classmethod
+    def on_paste(cls, a, b):
+        print("a")
+
+        Terminal.focused.paste_clipboard()
 
     def run(self, command: list[str], working_dir: Optional[str] = None):
         self.spawn_async(
